3c68996d839eb40ef158b3c6e935b10f8afbc827
David Allen <davidjallen11@gmail.com>
2021-03-25 22:50:29 -0600

    reorganizing and commenting

--- Lab2/TaskScheduler.cpp
+++ Lab2/TaskScheduler.cpp
@@ -9,8 +9,7 @@ TaskScheduler::TaskScheduler(std::string inFile, std::string outFile)
 	std::ifstream fileInStream (inFile);;
 	std::ofstream  fileOutStream (outFile);
 
-	
-	if (!fileInStream.is_open())
+	if (!fileInStream.is_open()) // Print an error message if the input file did not open correctly
 	{
 		std::cout << "input file did not open" << std::endl;
 	}
@@ -19,35 +18,38 @@ TaskScheduler::TaskScheduler(std::string inFile, std::string outFile)
 	int simTimeMs = 0;
 	int execTime;
 	int period;
+	int execTime = 0;
+	int period = 0;
+	int id = 0;
+	std::string line;
+	std::getline(fileInStream, line);
+	std::string idName = "";
+	std::string tempString;
+	
+	// Parse the first two variables from the input file
 	fileInStream >> numTasks;
 	fileInStream >> simTimeMs;
+	
+	// Print the variables just parsed
 	std::cout << "NumTasks: " << numTasks << " simTimeMs: " << simTimeMs << std::endl;
-	int id = 0;
+
 	while (!fileInStream.eof() && id < 20)
 	{
-		std::string line;
-		std::getline(fileInStream, line);
-		int execTime = 0;
-		int period = 0;
-		std::string idName = "";
-		std::string tempString;
-		int parsedFields = sscanf(line.c_str(), "%c, %d, %d", &tempString, &execTime, &period);
-    
-		//if (parsedFields == 3)
-		//{
-			idName = std::string(tempString);
-			fileInStream >> idName;
-			fileInStream >> execTime;
-			fileInStream >> period;
-			std::cout << "Created new task. ID: " << tempString << " ExecTime: " << execTime << " Period: " << period << std::endl;
-			//TaskEDF newTask = new TaskEDF(id, execTime, period);
-			//mTasks.push(newTask);
-		//}
-		//else
-		//{
-			std::cout << "did not parse the fields this time. ID: " << id << std::endl;
-			std::cout << "parsed only " << parsedFields << " fields." << std::endl;
-		//}
+
+		//int parsedFields = sscanf(line.c_str(), "%c, %d, %d", &tempString, &execTime, &period);
+
+		//Parse data from the input file
+		fileInStream >> numTasks;
+		fileInStream >> execTime;
+			
+		fileInStream >> idName; // = std::string(tempString);
+		fileInStream >> execTime;
+		fileInStream >> period;
+			
+		std::cout << "Created new task. ID: " << tempString << " ExecTime: " << execTime << " Period: " << period << std::endl;
+		//TaskEDF newTask = new TaskEDF(id, execTime, period);
+		//mTasks.push(newTask);
+		//std::cout << "parsed only " << parsedFields << " fields." << std::endl;
 		id++; // increment id
 	}
 }
