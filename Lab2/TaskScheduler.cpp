a1e156a1928e80107467274407a561ee0832caf2
David Allen <davidjallen11@gmail.com>
2021-03-25 22:27:10 -0600

    Input files open successfully. Beginning work on parsing file

--- Lab2/TaskScheduler.cpp
+++ Lab2/TaskScheduler.cpp
@@ -1,48 +1,58 @@
 #include "TaskScheduler.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
 
 TaskScheduler::TaskScheduler(std::string inFile, std::string outFile)
 {
-  std::ifstream fileInStream(inFile.c_str(), std::ifstream::in);
-  mScheduleOutput.open(outFile.c_str(), std::ofstream::out);
-  if (!fileInStream.is_open())
-  {
-    std::cout << "input file did not open" << std::endl;
-  }
-  int numTasks = 0;
-  int simTimeMs = 0;
-  fileInStream >> numTasks;
-  fileInStream >> simTimeMs;
-  std::cout << "NumTasks: " << numTasks << " simTimeMs: " << simTimeMs << std::endl;
-  int id = 0;
-  while (!fileInStream.eof() && id < 20)
-  {
-    std::string line;
-    std::getline(fileInStream, line);
-    int execTime = 0;
-    int period = 0;
-    std::string idName = "";
-    char tempString;
-    int parsedFields = sscanf(line.c_str(), "%c, %d, %d", &tempString, &execTime, &period);
-    if (parsedFields == 3)
-    {
-    //idName = std::string(tempString);
-    //fileInStream >> idName;
-    //fileInStream >> execTime;
-    //fileInStream >> period;
-    std::cout << "Created new task. ID: " << tempString << " ExecTime: " << execTime << " Period: " << period << std::endl;
-    //TaskEDF newTask = new TaskEDF(id, execTime, period);
-    //mTasks.push(newTask);
-    }
-    else
-    {
-      std::cout << "did not parse the fields this time. ID: " << id << std::endl;
-      std::cout << "parsed only " << parsedFields << " fields." << std::endl;
-    }
-    id++;
-  }
+	// Open Files
+	std::ifstream fileInStream (inFile);;
+	std::ofstream  fileOutStream (outFile);
+
+	
+	if (!fileInStream.is_open())
+	{
+		std::cout << "input file did not open" << std::endl;
+	}
+	
+	int numTasks = 0;
+	int simTimeMs = 0;
+	int execTime;
+	int period;
+	fileInStream >> numTasks;
+	fileInStream >> simTimeMs;
+	std::cout << "NumTasks: " << numTasks << " simTimeMs: " << simTimeMs << std::endl;
+	int id = 0;
+	while (!fileInStream.eof() && id < 20)
+	{
+		std::string line;
+		std::getline(fileInStream, line);
+		int execTime = 0;
+		int period = 0;
+		std::string idName = "";
+		std::string tempString;
+		int parsedFields = sscanf(line.c_str(), "%c, %d, %d", &tempString, &execTime, &period);
+    
+		//if (parsedFields == 3)
+		//{
+			idName = std::string(tempString);
+			fileInStream >> idName;
+			fileInStream >> execTime;
+			fileInStream >> period;
+			std::cout << "Created new task. ID: " << tempString << " ExecTime: " << execTime << " Period: " << period << std::endl;
+			//TaskEDF newTask = new TaskEDF(id, execTime, period);
+			//mTasks.push(newTask);
+		//}
+		//else
+		//{
+			std::cout << "did not parse the fields this time. ID: " << id << std::endl;
+			std::cout << "parsed only " << parsedFields << " fields." << std::endl;
+		//}
+		id++; // increment id
+	}
 }
 
 void TaskScheduler::runSchedule()
 {
-  std::cout << "Nothing here" << std::endl;
+	std::cout << "Nothing here" << std::endl;
 }
--- Lab2/scheduler.cpp
+++ Lab2/scheduler.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <string>
 #include "TaskScheduler.hpp"
+using namespace std;
 
 void printHelp()
-{
-  std::cout << "Help information" << std::endl;
-  exit(1);
+{	
+	std::cout << "Help information" << std::endl;
+	exit(1);
 }
 int main(int argc, char** argv)
 {
-  if (argc < 3) printHelp();
-  std::string inPath = argv[1];
-  std::string outPath = argv[2];
-  TaskScheduler* ts = new TaskScheduler(inPath, outPath);
-}
+	
+	//Process command line arguments
+	if (argc < 3) { //print instructions if the user did not enter the file names correctly
+		
+		std::printf("USAGE: %s <IN file> <OUT file> \n",argv[0]);
+		std::printf("<IN file> is the input file to be processed\n");
+		std::printf("<OUT file> is the output file to be generated\n");printHelp(); 	
+		return 0; //end program
+	}		
+	
+	std::string inPath = argv[1];
+	std::string outPath = argv[2];
+	TaskScheduler* ts = new TaskScheduler(inPath, outPath); //pass file names to Task Scheduler
+	
+	return 1;
+} // End of main function
