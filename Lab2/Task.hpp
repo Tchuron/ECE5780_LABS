e141d37d5a427b0cdf730e27959311512c41ea20
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2021-03-26 00:19:50 -0600

    Add a number of [untested] functions to the Task class

--- Lab2/Task.hpp
+++ Lab2/Task.hpp
@@ -1,29 +1,27 @@
+#include <memory>
+#include <climits>
+#include <iostream>
 class Task
 {
   public:
     // deadline is the release time for aperiodic and period for periodic
-    Task(char id, int execTime, int deadline, bool periodic)
-    {
-      mID = id;
-      mExecTime = execTime;
-      mExecAlready = 0;
-      mPeriodic = periodic;
-      if (periodic)
-      {
-        mRelease = 0;
-        mDeadline = deadline;
-      }
-      else
-      { //aperiodic
-        mRelease = deadline;
-        mDeadline = deadline + 500; // implicit 500ms deadline
-      }
-    }
+    Task(char id, int execTime, int deadline, bool periodic);
+    bool operator==(const Task& other);
+    bool operator!=(const Task& other);
+    bool isPeriodic();
+    int getDeadline();// for EDF scheduling
+    int getRmaPriority(int currentTime); // for RMA scheduling
+    char getID();
+    bool isReady(int currentTime);
+    bool isFinished();
+    void execute(int currentTime);
   private:
+    void periodicPushForward();
     char mID;
     int mExecTime;
     int mExecAlready;
     int mReleaseTime;
     int mDeadline;
     bool mPeriodic;
+    static std::shared_ptr<Task> mPreviouslyExecuted;
 };
