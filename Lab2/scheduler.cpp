46436c2c35fe51b58cb6477cff119412a01fb356
David Allen <davidjallen11@gmail.com>
2021-03-26 07:23:44 +0100

    updated files

--- Lab2/scheduler.cpp
+++ Lab2/scheduler.cpp
@@ -4,13 +4,22 @@
 
 void printHelp()
 {
-  std::cout << "Help information" << std::endl;
+	std::cout << "Help information" << std::endl;
+	printf("USAGE: ./Scheduler <IN file> <OUT file>");
+	printf("<IN file> is the input file to be processed\n");
+	printf("<OUT file> is the output file to be generated\n");
   exit(1);
 }
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  if (argc < 3) printHelp();
-  std::string inPath = argv[1];
-  std::string outPath = argv[2];
-  TaskScheduler* ts = new TaskScheduler(inPath, outPath);
+	
+	
+	if (argc < 3) {
+		printHelp();
+	}
+	else{
+		std::string inPath = argv[1];
+		std::string outPath = argv[2];
+		TaskScheduler* ts = new TaskScheduler(inPath, outPath);
+	}
 }
